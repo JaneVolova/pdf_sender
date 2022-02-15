745a080c81d9ff4a9b825f2931da56753783d38f
Jenya Volova <jane.volova.dev@gmail.com>
2022-02-15 02:02:21 +0200

    added structure NoteServlet

--- src/main/java/ru/blueteam/servlet/NotesServlet.java
+++ src/main/java/ru/blueteam/servlet/NotesServlet.java
@@ -1,8 +1,50 @@
 package ru.blueteam.servlet;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import ru.blueteam.repository.LogbookRepository;
+import ru.blueteam.repository.LogbookRepositoryImpl;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet
 public class NotesServlet extends HttpServlet {
+
+    private HikariDataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setUsername("postgres");
+        hikariConfig.setPassword("28679854");
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl("jdbc:postgresql://192.168.1.107:5432/pdfsender");
+        hikariConfig.setMaximumPoolSize(20);
+        System.out.println("Database connection inits success.");
+        this.dataSource = new HikariDataSource(hikariConfig);
+
+        System.out.println("Database has been connected.");
+        LogbookRepository logbookRepository = new LogbookRepositoryImpl(dataSource);
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
 }
